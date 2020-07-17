a9b62acfc711275cca83bdebe20a8d7f5d376d75
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-07-17 12:33:28 +0200

    Work in progress: add many real-plugins' configs.

--- z-a-meta-plugins.plugin.zsh
+++ z-a-meta-plugins.plugin.zsh
@@ -46,13 +46,91 @@ Zinit_Annex_Meta_Plugins_Map=(
 # The map in which the default sets of ices
 # for the real plugins are being stored.
 typeset -gA Zinit_Annex_Meta_Plugins_Config_Map
+typeset -g _std="lucid"
 Zinit_Annex_Meta_Plugins_Config_Map=(
-    sharkdp/fd            "null lucid from'gh-r' mv'fd* fd' sbin'**/fd'"
-    sharkdp/bat           "null lucid from'gh-r' mv'bat* bat' sbin'**/bat'"
-    sharkdp/hexyl         "null lucid from'gh-r' mv'hexyl* hexyl' sbin'**/hexyl'"
-    sharkdp/hyperfine     "null lucid from'gh-r' mv'hyperfine* hyperfine' sbin'**/hyperfine'"
-    sharkdp/vivid         "null lucid from'gh-r' mv'vivid* vivid' sbin'**/vivid'"
+    # @zinit-zsh (all annexes + extensions, without Meta-Plugins, obviously)
+    zinit-zsh/zinit-console     "$_std"
+    zinit-zsh/z-a-as-monitor    "$_std"
+    zinit-zsh/z-a-patch-dl      "$_std"
+    zinit-zsh/z-a-unscope       "$_std"
+    zinit-zsh/z-a-submods       "$_std"
+    zinit-zsh/z-a-rust          "$_std"
+    zinit-zsh/z-a-bin-gem-node  "$_std"
+    zinit-zsh/z-a-man           "$_std"
+    zinit-zsh/z-a-test          "$_std"
+
+    # @sharkdp
+    sharkdp/fd              "$_std null lucid from'gh-r' mv'fd* fd' sbin'**/fd'"
+    sharkdp/bat             "$_std null lucid from'gh-r' mv'bat* bat' sbin'**/bat'"
+    sharkdp/hexyl           "$_std null lucid from'gh-r' mv'hexyl* hexyl' sbin'**/hexyl'"
+    sharkdp/hyperfine       "$_std null lucid from'gh-r' mv'hyperfine* hyperfine' sbin'**/hyperfine'"
+    sharkdp/vivid           "$_std null lucid from'gh-r' mv'vivid* vivid' sbin'**/vivid'"
+    # @zsh-users
+    zsh-users/zsh-autosuggestions       "$_std atload'_zsh_autosuggest_start;'"
+    zsh-users/zsh-syntax-highlighting   "$_std atinit'ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay;'"
+    zsh-users/zsh-completions           "$_std pick'/dev/null'"
+    # @zdharma
+    zdharma/fast-syntax-highlighting    "$_std atinit'ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay;'"
+    zdharma/history-search-multi-word   "$_std atinit'zstyle :history-search-multi-word page-size 7;'"
+    zdharma/zsh-diff-so-fancy           "$_std null sbin'bin/git-dsf;bin/diff-so-fancy'"
+    # @zdharma, less popular
+    zdharma/zui             "$_std blockf"
+    zdharma/zconvey         "$_std sbin'cmds/zc-bg-notify;cmds/plg-zsh-notify'"
+    zdharma/zsh-unique-id   "$_std"
+    zdharma/zflai           "$_std"
+    github-issues           "$_std pack"
+    github-issues-srv       "$_std pack atinit'GIT_PROJECTS=zdharma/zinit GIT_SLEEP_TIME=700;'"
+    # @molovo
+    molovo/zunit            "$_std null sbin atclone'./build.zsh;' atpull'%atclone'"
+    molovo/color            "$_std null sbin'color.zsh -> color'"
+    molovo/revolver         "$_std as'program' pick'revolver'"
+    # @zpm-zsh
+    dircolors-material      "$_std pack"
+    # @pyenv
+    pyenv                   "$_std pack'bgn'"
+    # @ogham
+    ogham/exa               "$_std mv'exa* -> exa' sbin"
+    cargo-exa               "$_std null cargo='!exa' teleid'zdharma/null'"
+
+    # A few utility plugins
+    hlissner/zsh-autopair               "$_std"
+    urbainvaes/fzf-marks                "$_std"
+    psprint/zsh-navigation-tools        "$_std"
+    voronkovich/gitignore.plugin.zsh    "$_std trigger-load'!gi;!gii' \
+        dl'https://gist.githubusercontent.com/psprint/1f4d0a3cb89d68d3256615f247e2aac9/raw -> \
+            templates/Zsh.gitignore"
+    psprint/zsh-editing-workbench       "$_std atinit'local zew_word_style=whitespace;'"
+
+
+
+    # @marzocchi, a notifier, configured to use zconvey
+    marzocchi/zsh-notify      "$_std atinit'zstyle \":notify:*\" command-complete-timeout 3; \
+                                            zstyle \":notify:*\" notifier plg-zsh-notify"
+
+    # no username → a rust-annex usage to install Rust toolchain
+    rust-toolchain            "$_std null sbin='bin/*' rustup teleid'zdharma/null' \
+                                    atload='[[ ! -f \${ZINIT[COMPLETIONS_DIR]}/_cargo ]] && \
+                                        zi creinstall rust; \
+                                    export CARGO_HOME=\$PWD RUSTUP_HOME=\$PWD/rustup'"
+
+    # Fuzzy searchers
+    fzf     "$_std pack'bgn-binary'"
+    fzy     "$_std pack'bgn' git"
+
+    # Git extensions
+    Fakerr/git-recall         "$_std null sbin"
+    paulirish/git-open        "$_std null sbin"
+    paulirish/git-recent      "$_std null sbin"
+    davidosomething/git-my    "$_std null sbin"
+    arzzen/git-quick-stats    "$_std null sbin atload'export _MENU_THEME=legacy;'"
+    iwata/git-now             "$_std null sbin"
+    tj/git-extras             "$_std null make'PREFIX=$ZPFX'"
+
+    # @jonas
+    jonas/tig                 "$_std null make'prefix=$ZPFX install'"
+
 )
+unset _std
 
 
 # vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
