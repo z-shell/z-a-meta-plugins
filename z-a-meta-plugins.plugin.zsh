0a1d14a58c22e40cb587ee0ed2c61ef86617c365
Salvydas Lukosius <sall@w-ss.io>
2022-04-06 18:11:54 +0100

    Update z-a-meta-plugins.plugin.zsh

--- z-a-meta-plugins.plugin.zsh
+++ z-a-meta-plugins.plugin.zsh
@@ -14,7 +14,7 @@ typeset -gA Plugins
 Plugins[META_PLUGINS_DIR]="${0:h}"
 
 autoload -Uz za-meta-plugins-before-load-handler \
-za-meta-plugins-cmd-help-handler \
+za-meta-plugins-meta-cmd-help-handler \
 za-meta-plugins-default-ice-cmd 
 
 # An empty stub to fill the help handler fields
@@ -28,7 +28,7 @@ za-meta-plugins-help-null-handler "skip''" # Add a new ice
 # The subcommand `meta'.
 @zi-register-annex "z-a-meta-plugins" subcommand:meta \
 za-meta-plugins-default-ice-cmd  \
-za-meta-plugins-cmd-help-handler
+za-meta-plugins-meta-cmd-help-handler
 
 # The map in which the definitions of the meta-plugins are being stored.
 typeset -gA zi_annex_meta_plugins_map
