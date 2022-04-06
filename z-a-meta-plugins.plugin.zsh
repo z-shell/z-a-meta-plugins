e119db675872d7b66c5be92d6bf7231b50908b8b
Salvydas Lukosius <sall@w-ss.io>
2022-04-06 12:54:16 +0100

    Update z-a-meta-plugins.plugin.zsh

--- z-a-meta-plugins.plugin.zsh
+++ z-a-meta-plugins.plugin.zsh
@@ -101,9 +101,9 @@ typeset -g _std="lucid"
 zi_annex_meta_plugins_config_map=(
   # @z-shell (all annexes + extensions, without Meta-Plugins, obviously)
   z-shell/z-a-bin-gem-node  "$_std"
+  z-shell/z-a-readurl       "$_std"
   z-shell/z-a-default-ice   "$_std compile'*handler'"
   z-shell/z-a-patch-dl      "$_std compile'*handler'"
-  z-shell/z-a-readurl       "$_std compile'*handler'"
   z-shell/z-a-unscope       "$_std compile'*handler'"
   z-shell/z-a-submods       "$_std compile'*handler'"
   z-shell/z-a-linkbin       "$_std compile'*handler'"
