3d7b9713eccc14a6a0bd13fd13a6cbe168338449
Sall <sall@w-ss.io>
2023-01-03 08:11:11 +0000

    Update z-a-meta-plugins.plugin.zsh
    Signed-off-by: Sall <sall@w-ss.io>

--- z-a-meta-plugins.plugin.zsh
+++ z-a-meta-plugins.plugin.zsh
@@ -3,7 +3,9 @@
 #
 # Copyright (c) 2021 Z-Shell Community
 #
-# Zsh Plugin Standard
+builtin emulate -L zsh ${=${options[xtrace]:#off}:+-o xtrace}
+builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+
 # https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
